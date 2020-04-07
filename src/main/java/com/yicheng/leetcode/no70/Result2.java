896cc0997907e4dd525f90a76c820256c80ed07c
bluetie915 <1402711057@qq.com>
2020-04-07 17:34:05 +0800

    题目解析

---
+++ src/main/java/com/yicheng/leetcode/no70/Result2.java
@@ -0,0 +1,21 @@
+package com.yicheng.leetcode.no70;
+
+public class Result2 {
+
+    public static void main(String[] args) {
+        int result = climbStairs(6);
+        System.out.println(result);
+    }
+
+    public static int climbStairs(int n) {
+        if (n < 3)
+            return n;
+        int f1 = 1, f2 = 2, f3 = 3;
+        for (int i = 3 ; i <= n; i ++) {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
+    }
+}
