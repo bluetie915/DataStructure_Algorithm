0995e7478edf9385e614f75f36756b1d436bd77d
bluetie915 <1402711057@qq.com>
2020-04-16 22:37:44 +0800

    题目解析

--- src/main/java/com/yicheng/test/Test2.java
+++ src/main/java/com/yicheng/test/Test2.java
@@ -1,15 +1,15 @@
-package com.yicheng.test;
-
-public class Test2 {
-
-    public static void main(String[] args) {
-        int result = solution(32);
-        System.out.println(result);
-    }
-
-    public static int solution(int n) {
-        String m = "" + n;
-        int temp = -1;
-        return Integer.parseInt(m.toString());
-    }
-}
+//package com.yicheng.test;
+//
+//public class Test2 {
+//
+//    public static void main(String[] args) {
+//        int result = solution(32);
+//        System.out.println(result);
+//    }
+//
+//    public static int solution(int n) {
+//        String m = "" + n;
+//        int temp = -1;
+//        return Integer.parseInt(m.toString());
+//    }
+//}
