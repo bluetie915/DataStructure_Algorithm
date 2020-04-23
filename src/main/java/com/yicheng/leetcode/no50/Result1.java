956cd86e2e73e99effaa150f925927dd05f5b9ab
bluetie915 <1402711057@qq.com>
2020-04-23 23:29:33 +0800

    题目解析

---
+++ src/main/java/com/yicheng/leetcode/no50/Result1.java
@@ -0,0 +1,26 @@
+package com.yicheng.leetcode.no50;
+
+/**
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+ */
+
+public class Result1 {
+
+    public static void main(String[] args) {
+        double result = myPow(2, -2);
+        System.out.println(result);
+    }
+
+    public static double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1;
+        for (int i = 0; i < N; i++) {
+            result = result * x;
+        }
+        return result;
+    }
+}
