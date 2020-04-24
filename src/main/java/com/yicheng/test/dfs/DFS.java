027ef4c0a1f83646ba2d8920f96d931ef00081d6
bluetie915 <1402711057@qq.com>
2020-04-24 18:01:52 +0800

    题目解析

---
+++ src/main/java/com/yicheng/test/dfs/DFS.java
@@ -0,0 +1,32 @@
+package com.yicheng.test.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFS {
+
+    public static void main(String[] args) {
+        DFS result1 = new DFS();
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(5);
+        node.right = new TreeNode(8);
+        node.left.left = new TreeNode(1);
+        node.left.right = new TreeNode(9);
+        node.right.right = new TreeNode(4);
+        node.right.right.left = new TreeNode(7);
+        List<Integer> result = result1.levelOrder(node);
+        System.out.println(result);
+    }
+
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> levelOrder(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        list.add(root.val);
+        levelOrder(root.left);
+        levelOrder(root.right);
+        return list;
+    }
+}
---
+++ src/main/java/com/yicheng/test/dfs/TreeNode.java
@@ -0,0 +1,11 @@
+package com.yicheng.test.dfs;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
