590d4902d4f2d14f6c98b657d4da4c2394b601fe
bluetie915 <1402711057@qq.com>
2020-05-27 23:46:27 +0800

    题目解析

--- src/main/java/com/yicheng/leetcode/no2/Result1.java
+++ src/main/java/com/yicheng/leetcode/no2/Result1.java
@@ -1,23 +1,62 @@
 package com.yicheng.leetcode.no2;
 
+import java.awt.*;
+
+/**
+ * 给出两个非空的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
 public class Result1 {
 
-    ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        StringBuffer s1 = new StringBuffer();
-        StringBuffer s2 = new StringBuffer();
-        s1.append(l1.val);
-        s2.append(l2.val);
-        while (l1.next != null)
-            s1.append(l1.next.val);
-        while (l2.next != null)
-            s2.append(l2.next.val);
-        System.out.println(s1);
-        return null;
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(2);
+        listNode1.next = new ListNode(4);
+        listNode1.next.next = new ListNode(3);
+        System.out.println(printNode(listNode1));
+
+        ListNode listNode2 = new ListNode(5);
+        listNode2.next = new ListNode(6);
+        listNode2.next.next = new ListNode(4);
+        System.out.println(printNode(listNode2));
+        ListNode result = addTwoNumbers(listNode1, listNode2);
+        String s = printNode(result);
+        System.out.println(s);
+    }
+
+    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    static String printNode(ListNode listNode) {
+        if (listNode.next == null)
+            return listNode.val+"";
+        return (listNode.val + printNode(listNode.next)) + "";
     }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
---
+++ src/main/java/com/yicheng/leetcode/no3/Result1.java
@@ -0,0 +1,40 @@
+package com.yicheng.leetcode.no3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ */
+
+public class Result1 {
+
+    public static void main(String[] args) {
+        int result = lengthOfLongestSubstring(" ");
+        System.out.println(result);
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0 || s.trim().length() == 0)
+            return 0;
+        Map<Character, Integer> map;
+        int currLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            map = new HashMap<Character, Integer>();
+            for (int j = i; j < s.length(); j++) {
+                if (map.containsKey(s.charAt(j))) {
+                    currLength = Integer.max(map.size(), currLength);
+                    break;
+                }
+                map.put(s.charAt(j), j);
+            }
+        }
+        return currLength;
+    }
+}
