49389c25f4103a1d3e99ede74f03527bc41b7aac
bluetie915 <1402711057@qq.com>
2020-05-30 22:53:23 +0800

    题目解析

--- src/main/java/com/yicheng/leetcode/no680/Result1.java
+++ src/main/java/com/yicheng/leetcode/no680/Result1.java
@@ -7,28 +7,22 @@ package com.yicheng.leetcode.no680;
 public class Result1 {
 
     public static void main(String[] args) {
-        boolean result = validPalindrome("abc");
+        boolean result = validPalindrome("ayt2ya");
         System.out.println(result);
     }
 
     public static boolean validPalindrome(String s) {
-        if (s == null)
-            return false;
-        int chance = 1;
-        int i = 0, j = s.length() - 1;
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int i, int j) {
         while (i < j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
-            } else if (chance == 1 && s.charAt(i + 1) == s.charAt(j)) {
-                i += 2;
-                j--;
-                chance--;
-            } else if (chance == 1 && s.charAt(i) == s.charAt(j + 1)) {
-                i++;
-                j -= 2;
-                chance--;
-            } else {
+            if (s.charAt(i++) != s.charAt(j--)) {
                 return false;
             }
         }
