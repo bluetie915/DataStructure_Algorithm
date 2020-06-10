bc4ed4a8c4f0e638f87c983d1d9e0fd1778eb053
bluetie915 <1402711057@qq.com>
2020-06-10 21:34:23 +0800

    题目解析

---
+++ src/main/java/com/yicheng/leetcode/no575/Result2.java
@@ -0,0 +1,21 @@
+package com.yicheng.leetcode.no575;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Result2 {
+
+    public static void main(String[] args) {
+        int[] candies = new int[]{1, 1, 2, 2, 3, 3};
+        int result = distributeCandies(candies);
+        System.out.println(result);
+    }
+
+    public static int distributeCandies(int[] candies) {
+        Set<Integer> set = new HashSet<>();
+        for (int candy : candies) {
+            set.add(candy);
+        }
+        return Math.min(set.size(), candies.length / 2);
+    }
+}
